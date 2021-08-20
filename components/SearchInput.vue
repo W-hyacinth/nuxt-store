07b5434a769dd9984b355258c3f4431b3cbe7550
W-hyacinth <w_hyacinth.choi@kakao.com>
2021-08-20 21:32:24 +0900

    - class 위치가 @click 이벤트 뒤에 위치하면서 발생하는 eslint 오류 개선

--- components/SearchInput.vue
+++ components/SearchInput.vue
@@ -3,7 +3,7 @@
     <h2 class="ally">검색영역</h2>
     <div class="search__field">
       <input type="text" :value="value" class="search__input" placeholder="검색어를 입력하세요" @input="$emit('input', $event.target.value)">
-      <button type="submit" @click.prevent="$emit('search')" class="search__btnSubmit">검색</button>
+      <button type="submit" class="search__btnSubmit" @click.prevent="$emit('search')">검색</button>
     </div>
   </section>
 </template>
